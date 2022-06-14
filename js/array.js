04f1a62b258902d86fec9773d3e33bd079387df2
BruLekichLouro <bububio@yahoo.com.br>
2022-06-14 13:50:08 -0300

    Adicionando mais informações sobre array

--- js/array.js
+++ js/array.js
@@ -37,13 +37,32 @@ persons.forEach((person, index, arr) =>{
 
 //Filtrar array
 //Funções filter, map e reduce não alteram a referência dos objetos, retornam um novo a partir do original
-const mens = persons.filter(person => person.gender === gender.Man);
+const mens = persons.filter(person => person.gender === gender.Man); //filter é condicional
 console.log('\n Nova lista apenas com homens:', mens);
 
 //Retornar um novo
 const personsWithCourse = persons.map(person => {
-    person.course = 'Introdução ao JS';
-    return person;
+    person.course = 'Introdução ao JS'; //permite que retorne um novo array de cursos e até adicione propriedade
+    return person; 
 });
 console.log('\n Pessoas com a adição do course:', personsWithCourse);
+
+//Transformar um array em outro tipo
+const totalAge = persons.reduce((age, person) => { //age é a variável que vou retornar
+    age += person.age; // soma a idade de cada pessoa na variável e retorna um number
+    return age; //retorna um number
+}, 0); //valor inicial da minha propriedade, age = 0;
+//Acima a gente vê que o primeiro parametro que recebe é uma função e segundo é o valor da propriedade age
+console.log('\n Soma das idades das pessoas', totalAge);
+
+//Existem outras funções que retornam arrays, podemos encadear a execução delas
+//Juntando operções:
+const totalEvenAges = persons
+                            .filter(person => person.age % 2 ===0) //filtrando
+                            .reduce((age, person) => {  //transformando em numero
+                                age+=person.age;
+                                return age;
+                            }, 0);
+
+    console.log('\n Soma das idades das idades das pessoas com idade par:', totalEvenAges);
 
