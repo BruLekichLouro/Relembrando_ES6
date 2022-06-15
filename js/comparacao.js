bf4f2d496e9fc9779571f23af3957176043c5a00
BruLekichLouro <bububio@yahoo.com.br>
2022-06-15 14:55:33 -0300

    Adicionando arquivo para conteúdo sobre comparação

---
+++ js/comparacao.js
@@ -0,0 +1,28 @@
+//Igualdade (==)
+// Retorna verdadeiro caso os operadores seja iguais
+"3" == var1
+3 == "3"
+
+// Não igual (!=)
+//retorna verdadeiro caso os operadores não sejam iguais
+var2 != "3"
+
+//Estritamente igual - valor e tipo (===)
+//Retorna verdadeiro caso os operadores sejam iguais e do mesmo tipo, 
+//veja tb Object.is e igualdade
+3 === var1
+
+//Estritamente não igual (!==)
+//Retorna verdadeiro caso os operadores não sejam iguais e/ou não sejam do mesmo tipo
+var1 !== "3"
+3 !== "3"
+
+//Maior que (>)
+//Retorna verdadeiro caso o operando da esquerda seja maior que o da direita
+var2 > var1
+"12"> 2
+
+//Maior que ou igual(>=)
+//Retorna verdadeiro caso o operando da esquerda seja maior ou igual da direita 
+var2 >= var1
+var1 >= 3
