98264d886ec979d4b517ff2ab9517557dc2916b6
BruLekichLouro <bububio@yahoo.com.br>
2022-06-15 14:28:11 -0300

    Adicionando arquivo com conteúdo

---
+++ js/op_aritmeticos.js
@@ -0,0 +1,31 @@
+//Modulo (%)
+//Operador binário. Retorna o inteiro restante da divisão dos dois operadores
+
+12 % 5 //retorna 2
+
+//Incremento(++) e decremento(--)
+x++
+++x
+//Nos 2 cenários o valor será incrementado:
+const a = ++2; //a já recebe o valor incrementado
+const b = 2++; // b recebe o valor atual 
+
+x-- //mesma explicação que no de cima
+--x
+
+//Negação e adição:
+-3 //esta negando o 3
++"3" //retorna 3 (o mais força conversão de string para number)
++true //retorna 1
++false //retorna 0
+-true //retorna -1
+
+//Operador de exponenciação(**)
+2**3 //retorna 8
+10**-1 //retorna 0
+
+//Operador de agrupamento ():
+2*(3 + 2) //preciso que a soma seja executada antes da multiplicaçao
+
+//Operador de agrupamento ()
+2*(3+2)
