f1d5baca8b11355508e3d2f92b017870c89dcc76
BruLekichLouro <bububio@yahoo.com.br>
2022-06-15 17:08:40 -0300

    Adicionando arquivo de conteúdo e script sobre operadores lógicos

---
+++ js/logico.js
@@ -0,0 +1,9 @@
+//AND (e) lógico &&
+//OR (ou) lógico ||
+//NOT lógico (!)
+var n1 = !true; //retorna false
+var n2 = !false; //retorna true
+var n3 = !"Gato"; // retorna false
+//strings normalmente retornam true
+//true: " " retorna 1
+//false: "" retorna false 0
